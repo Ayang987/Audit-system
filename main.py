4c21ee2df233359bb78e7a8b1628965d5d2befc3
Ayang987 <2272614525@qq.com>
2023-12-13 14:53:10 +0800

    1

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import hashlib
 import os
 import pickle
-import chat_main
-
-
 class MerkleNode:
-    def __init__(self, hash_value, left=None, right=None):
+    def __init__(self, hash_value, raw_files, left=None, right=None):
         self.hash_value = hash_value
         self.left = left
         self.right = right
-
+        self.raw_files = raw_files
 
 def calculate_file_hash(file_path):
     sha512 = hashlib.sha512()
@@ -18,13 +15,12 @@ def calculate_file_hash(file_path):
             sha512.update(chunk)
     return sha512.hexdigest()
 
-
 def build_merkle_tree(file_path):
     # Read the file and build a list of leaf nodes
-    leaf_nodes = [MerkleNode(calculate_file_hash(file_path[0])),
-                  MerkleNode(hash_value=calculate_file_hash(file_path[1])),
-                  MerkleNode(hash_value=calculate_file_hash(file_path[2])),
-                  MerkleNode(hash_value=calculate_file_hash(file_path[3]))]
+    leaf_nodes = [MerkleNode(hash_value=calculate_file_hash(file_path[0]), raw_files=[file_path[0]]),
+                  MerkleNode(hash_value=calculate_file_hash(file_path[1]), raw_files=[file_path[1]]),
+                  MerkleNode(hash_value=calculate_file_hash(file_path[2]), raw_files=[file_path[2]]),
+                  MerkleNode(hash_value=calculate_file_hash(file_path[3]), raw_files=[file_path[3]])]
 
     # Build the Merkle Tree bottom-up
     while len(leaf_nodes) > 1:
@@ -34,49 +30,26 @@ def build_merkle_tree(file_path):
             right = leaf_nodes[i + 1] if i + 1 < len(leaf_nodes) else None
             combined_hash = hashlib.sha512((left.hash_value + (right.hash_value if right else '')).encode()).hexdigest()
 
-            new_node = MerkleNode(hash_value=combined_hash, left=left, right=right)
+            new_node = MerkleNode(hash_value=combined_hash, raw_files=left.raw_files + right.raw_files, left=left,
+                                  right=right)
             new_level.append(new_node)
         leaf_nodes = new_level
 
     return leaf_nodes[0]  # Return the root of the Merkle Tree
 
 
-def load_merkle_tree(load_path):
-    with open(load_path, 'rb') as f:
-        return pickle.load(f)
+def save_merkle_tree(root_node, save_path):
+    with open(save_path, 'wb') as f:
+        print(root_node)
+        pickle.dump(root_node, f)
 
 
-def verify_file_integrity(file_path, merkle_tree_root):
-    current_hash = build_merkle_tree(file_path)
-    return verify_merkle_tree(merkle_tree_root, current_hash)
-
-def verify_merkle_tree(node, re_node):
-    if node is None:
-        return True
-    try:
-        combined_hash = hashlib.sha512(
-            (node.left.hash_value + (node.right.hash_value if node.right else '')).encode()).hexdigest()
-        print(f"combined_hash={combined_hash}")
-        re_combined_hash = hashlib.sha512(
-            (re_node.left.hash_value + (re_node.right.hash_value if re_node.right else '')).encode()).hexdigest()
-        print(f"re_combined_hash={re_combined_hash}")
-        if combined_hash != re_combined_hash:
-            return False
-    except:
-        pass
-    return verify_merkle_tree(node.left, re_node.left) and verify_merkle_tree(node.right, re_node.right)
-
-
+# Example usage:
 def main():
-    # Step 2: Verify file integrity
+    # Step 1: Build Merkle Tree and save it
     file_path = ["111.txt", "222.txt", "新建文本文档.txt", "example.txt"]
-    loaded_merkle_tree = load_merkle_tree('merkle_tree.pkl')
-    result = verify_file_integrity(file_path, loaded_merkle_tree)
-
-    if result:
-        print("File integrity verified successfully.")
-    else:
-        print("File integrity verification failed.")
+    merkle_tree_root = build_merkle_tree(file_path)
+    save_merkle_tree(merkle_tree_root, 'merkle_tree.pkl')
 
 
 if __name__ == "__main__":
